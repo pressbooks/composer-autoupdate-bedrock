dee5d3581d035d39de8e3ec6d881d7c207fdfb05
arzola <oscararzola@gmail.com>
2022-11-23 14:28:07 -0500

    feat: add octokit rest api

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { Octokit } = require("@octokit/action");
+
+// TODO: set JSON secret in repo settings
+const reposToCallAction = [
+    'uses-updater',
+];
 
 try {
     const trigger = core.getInput('triggered-by');
+    const token = core.getInput('token');
+    const octokit = new Octokit({
+        auth: token,
+    });
     console.log(`Triggered by ${trigger}!`);
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    for (let repo in reposToCallAction) {
+        console.log(`Calling createWorkflowDispatch on ${repo}`);
+        octokit.rest.actions.createWorkflowDispatch({
+            owner: 'pressbooks',
+            repo: repo,
+            workflow_id: 'autoupdate.yml',
+            ref: 'main',
+        }).then((response) => {
+            console.log(`Github API response: ${response}`);
+        });
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
