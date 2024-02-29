7ac1f5a7e33f37fe62fc483d03652f52caa89b47
Ho Man Chan <homan98@gmail.com>
2024-02-29 10:55:07 -0500

    feat: Improve composer autoupdate behaviour (#74)
    * chore: update index.js to update based on repo name *-bedrock private using organization repo with github api
    
    * feat: update index.js to list all bedrocks and trigger workflow to all define branches
    feat: update index.js to send workflow with the desired package instead of all pressbooks packages
    feat: update auto-update.yml to accept input package and prevent concurrent runs when updating multiple packages
    feat: add test folder to run index.js locally
    
    * chore: update dev branches to use test branch of bedrock
    
    * chore: add check to see if the repo is in composer.json before dispatching
    
    * chore: add checks to ensure the bedrock has the branch and repo before triggering action
    
    * chore: build code
    
    * chore: add fetch to support lower node version
    
    * fix: dispatch using repo name and not the entire object
    
    * chore: clean up code
    
    * chore: remove testing bedrock trigger only case
    
    * chore: update readme.md instruction for usage and release
    chore: update console log message when dispatching workflow with more clarity

--- index.js
+++ index.js
@@ -1,32 +1,132 @@
+const fetch = require('node-fetch');
 const core = require('@actions/core');
-const {Octokit} = require("@octokit/action");
+const { Octokit: ActionOctokit } = require("@octokit/action");
+const { Octokit: RestOctokit } = require("@octokit/rest");
 
-const reposToDispatchComposerUpdate = [
-    'pressbooksedu-golden-bedrock',
-    'pressbookspublic-bedrock',
-    'pressbookspub-bedrock'
-];
+const isGitHubActions = process.env.GITHUB_ACTION || false;
+const owner = 'pressbooks';
+const bedrockBranchTarget = {
+  'dev': ['dev', 'lti-development'],
+  'staging': ['staging'],
+  'production': ['production']
+};
 
-try {
-    const trigger = core.getInput('triggered-by');
-    const token = core.getInput('token');
-    let branch = core.getInput('branch');
-    branch === 'refs/heads/production' ? branch = 'staging' : branch = 'dev';
-    const octokit = new Octokit({
-        auth: token,
-    });
+const trigger = isGitHubActions ? core.getInput('triggered-by') : process.env.INPUT_TRIGGERED_BY || 'default-trigger';
+const token = isGitHubActions ? core.getInput('token') : process.env.INPUT_TOKEN || 'default-token';
+let branch = isGitHubActions ? core.getInput('branch') : process.env.INPUT_BRANCH || 'refs/heads/dev';
+branch === 'refs/heads/production' ? branch = 'staging' : branch = 'dev';
+
+const actionOctokit = new ActionOctokit({
+  auth: token,
+  request: {
+    fetch: fetch,
+  },
+});
+
+const restOctokit = new RestOctokit({
+  auth: token,
+  request: {
+    fetch: fetch,
+  },
+});
+
+dispatchWorkflow();
+
+async function dispatchWorkflow() {
+  try {
+    const reposToDispatchComposerUpdate = await listBedrockRepos(owner);
+
     console.log(`Triggered by ${trigger}!`);
-    for (const repo of reposToDispatchComposerUpdate) {
-        console.log(`Calling createWorkflowDispatch on ${repo}`);
-        octokit.rest.actions.createWorkflowDispatch({
-            owner: 'pressbooks',
-            repo: repo,
-            workflow_id: 'autoupdate.yml',
-            ref: branch,
-        }).then((response) => {
-            console.log(`Github API response: ${response}`);
-        });
+    for (repo of reposToDispatchComposerUpdate) {
+      console.log(`Calling createWorkflowDispatch on ${repo.name}`);
+      for (branchValue of bedrockBranchTarget[branch]) {
+        console.log(`in branch ${branchValue}`);
+        if (
+          await checkBranchExists(repo.name, branchValue) &&
+          await checkComposerPackage(trigger, 'pressbooks', repo.name, 'composer.json', branchValue)
+        ) {
+          console.log(`Dispatched update on ${repo.name} in branch ${branchValue} for ${trigger} package`)
+          actionOctokit.rest.actions.createWorkflowDispatch({
+              owner: owner,
+              repo: repo.name,
+              workflow_id: 'autoupdate.yml',
+              ref: branchValue,
+              inputs: {
+                package: trigger,
+              }
+          }).then((response) => {
+              console.log(`Github API response: ${response}`);
+          });
+        }
+      }
     }
-} catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
+  }
+}
+
+async function listBedrockRepos(organization) {
+  try {
+    let allRepos = [];
+    let page = 1;
+    let reposResponse = [];
+
+    do {
+      reposResponse = await restOctokit.rest.repos.listForOrg({
+        org: organization,
+        type: 'private',
+        per_page: 100,
+        page,
+      });
+      allRepos.push(...reposResponse.data);
+      page++;
+    } while (reposResponse.data.length === 100);
+
+    const bedrockRepos = allRepos.filter(repo => repo.name.endsWith('-bedrock'));
+
+    console.log('Repositories ending with -bedrock:', bedrockRepos.map(repo => repo.name));
+    return bedrockRepos;
+  } catch (error) {
+    console.error('Error fetching repositories:', error);
+  }
+}
+
+async function checkComposerPackage(packageName, owner, repo, path, branch) {
+  try {
+    const { data } = await restOctokit.repos.getContent({
+      owner,
+      repo,
+      path,
+      ref: branch,
+    });
+
+    const content = Buffer.from(data.content, 'base64').toString();
+    const composerJson = JSON.parse(content);
+    const packageFound = composerJson.require && composerJson.require[packageName] !== undefined;
+
+    console.log(`Package ${packageName} found: ${packageFound}`);
+    return packageFound;
+  } catch (error) {
+    console.error('Error fetching composer.json:', error.message);
+  }
+}
+
+async function checkBranchExists(repo, branch) {
+  try {
+    const { data } = await restOctokit.rest.repos.getBranch({
+      owner,
+      repo,
+      branch,
+    });
+
+    console.log(`Branch ${branch} exists in ${owner}/${repo}`);
+    return true;
+  } catch (error) {
+    if (error.status === 404) {
+      console.log(`Branch ${branch} does not exist in ${owner}/${repo}`);
+    } else {
+      console.error(`Error checking branch existence: ${error.message}`);
+    }
+    return false;
+  }
 }
