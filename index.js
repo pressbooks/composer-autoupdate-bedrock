5f47801e1f0fb2f98d15bc020b0c2a6b695dc149
Ho Man Chan <homan98@gmail.com>
2024-03-19 09:29:17 -0400

    docs: update readme with new instructions and inputs chore: remove unneed action for github with inputs chore: update dependencies fix: rename variables using SQS to SNS

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const AWS = require('aws-sdk');
-// const core = require('@actions/core');
 
-// const isGitHubActions = process.env.GITHUB_ACTION || false;
 const trigger = process.env.INPUT_TRIGGERED_BY || 'default-trigger';
 let branch = process.env.INPUT_BRANCH || 'refs/heads/dev';
 branch === 'refs/heads/production' ? branch = 'staging' : branch = 'dev';
 
-let topicArn =  (branch === 'dev') ? process.env.AWS_SQS_ARN_DEV : process.env.AWS_SQS_ARN_PROD;
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'ca-central-1',
-});
-
-const sns = new AWS.SNS({apiVersion: '2010-03-31'});
+const topicArn =  (branch === 'dev') ? process.env.AWS_SNS_ARN_DEV : process.env.AWS_SNS_ARN_STAGING;
 
 const message = {"Message": trigger};
 
@@ -23,6 +13,14 @@ const params = {
   TopicArn: topicArn,
   MessageGroupId: 'github-repo-update',
 };
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY,
+  region: 'ca-central-1',
+});
+
+const sns = new AWS.SNS({apiVersion: '2010-03-31'});
 
 console.log("params: ", params);
 sns.publish(params, function(err, data) {
