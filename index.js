aef4749196da72898c5b8d9f83d2b940594315f3
Ho Man Chan <homan98@gmail.com>
2024-04-11 07:53:47 -0400

    chore: add debug log

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ let branch = process.env.INPUT_BRANCH || 'refs/heads/dev';
 branch === 'refs/heads/production' ? branch = 'staging' : branch = 'dev';
 
 const topicArn =  (branch === 'dev') ? process.env.AWS_SNS_ARN_DEV : process.env.AWS_SNS_ARN_STAGING;
-
+console.log(topicArn);
 const message = {"Message": trigger};
-
-const params = {
-  Message: JSON.stringify(message),
-  TopicArn: topicArn,
-  MessageGroupId: 'github-repo-update',
-};
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'ca-central-1',
-});
-
-const sns = new AWS.SNS({apiVersion: '2010-03-31'});
-
-console.log("params: ", params);
-sns.publish(params, function(err, data) {
-  if (err) {
-    console.error("Error sending message: ", err);
-  } else {
-    console.log("Message sent successfully: ", data.MessageId);
-  }
-});
+//
+// const params = {
+//   Message: JSON.stringify(message),
+//   TopicArn: topicArn,
+//   MessageGroupId: 'github-repo-update',
+// };
+//
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: 'ca-central-1',
+// });
+//
+// const sns = new AWS.SNS({apiVersion: '2010-03-31'});
+//
+// console.log("params: ", params);
+// sns.publish(params, function(err, data) {
+//   if (err) {
+//     console.error("Error sending message: ", err);
+//   } else {
+//     console.log("Message sent successfully: ", data.MessageId);
+//   }
+// });
