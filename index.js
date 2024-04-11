b9525065f9d25a4340ddc5a45fa34ed4bcbc697b
Ho Man Chan <homan98@gmail.com>
2024-04-11 08:04:36 -0400

    chore: update build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 
 const trigger = process.env.INPUT_TRIGGERED_BY || 'default-trigger';
 let branch = process.env.INPUT_BRANCH || 'refs/heads/dev';
+console.log(process.env.INPUT_BRANCH);
 console.log(branch);
 branch === 'refs/heads/production' ? branch = 'staging' : branch = 'dev';
 console.log(branch);
